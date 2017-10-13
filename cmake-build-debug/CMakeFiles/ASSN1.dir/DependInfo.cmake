201706ca78400669803a7ab97c2c7e29a6dedb69
Flora1888 <avril_99@vii.im>
2017-10-13 07:15:27 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ASSN1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Flora/CLionProjects/PIC10C/ASSN1/cards.cpp" "C:/Users/Flora/CLionProjects/PIC10C/ASSN1/cmake-build-debug/CMakeFiles/ASSN1.dir/cards.cpp.obj"
+  "C:/Users/Flora/CLionProjects/PIC10C/ASSN1/main.cpp" "C:/Users/Flora/CLionProjects/PIC10C/ASSN1/cmake-build-debug/CMakeFiles/ASSN1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ASSN1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ASSN1.dir/main.cpp.obj"
+  "CMakeFiles/ASSN1.dir/cards.cpp.obj"
+  "ASSN1.pdb"
+  "ASSN1.exe"
+  "ASSN1.exe.manifest"
+  "libASSN1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ASSN1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
