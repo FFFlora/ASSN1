354b4161abde74093e9817709375204b61d42efa
Flora1888 <avril_99@vii.im>
2017-12-13 14:35:20 -0800

    second commit

--- cards.cpp
+++ cards.cpp
@@ -5,7 +5,10 @@
 #include "cards.h"
 #include <cstdlib>
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
 
+using namespace std;
 /*
 You might or might not need these two extra libraries
 #include <iomanip>
@@ -209,21 +212,89 @@ int Card::get_rank() const {
 /*    enum rank {
         As = 1, Dos = 2, Tres = 3, Cuatro = 4, Cinco = 5, Seis = 6, Siete = 7, Sota = 10, Caballo = 11, Rey = 12
     };*/
-    return static_cast<int>(rank);
+    return static_cast<int>(rank)+1;
 }
 
-
+double Card::get_value() const {
+    int rank = get_rank();
+    if ((rank == 10) || (rank == 11) || (rank == 12)) {
+        return 0.5;
+    }
+    else {
+        return double(rank);
+    }
+}
 // Comparison operator for cards
 // Returns TRUE if card1 < card2
 bool Card::operator<(Card card2) const {
     return rank < card2.rank;
 }
 
+//Prints a card's information in Spanish and English
+void Card::printCards() const {
+    cout << "\t" << get_spanish_rank() << " de " << get_spanish_suit();
+    cout << " (" << get_english_rank() << " of " << get_english_suit() << ")" << endl;
+}
+
+
+
 /* *************************************************
    Hand class
    ************************************************* */
-// Implemente the member functions of the Hand class here.
 
+//Adds a new card to the hand
+void Hand::addCard(Card C) {
+    cards.push_back(C);
+}
+
+//Prints the information of all cards in the hand
+void Hand::printCards() const {
+    for (int i = 0; i < cards.size(); i++) {
+        cards[i].printCards();
+    }
+}
+
+//Returns the total combined value of all cards in the hand
+double Hand::get_total() const {
+    double total = 0;
+    for (int i = 0; i < cards.size(); i++) {
+        total += cards[i].get_value();
+    }
+    return total;
+}
+
+//Returns true if the hand has busted (its value is over 7.5)
+bool Hand::bust() const {
+    if (get_total() > 7.5) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+}
+
+/* Player class */
+
+//Constructor which assigns a given amount of money
+Player::Player(int m){
+    money = m;
+}
+
+//Returns the amount of money the player has
+int Player::get_money() const {
+    return money;
+}
+
+//Increases the player's money by a given amount
+void Player::gain(int money_to_gain) {
+    money += money_to_gain;
+}
+
+//Decreases the player's money by a given amount
+void Player::lose(int money_to_lose) {
+    money -= money_to_lose;
+}
 
 
 /* *************************************************
--- siete_y_medio.cpp
+++ siete_y_medio.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 // Global constants (if any)
 
+const int STARTING_MONEY = 100; //amount of money with which the player starts
+const double DEALER_THRESHOLD = 5.5; //the total hand value at which the dealer will stop drawing another card
 
 // Non member functions declarations (if any)
 
@@ -18,6 +20,110 @@ using namespace std;
 
 // Stub for main
 int main(){
-    cout<<"all passed.";
+
+
+    Player myPlayer = Player(STARTING_MONEY);
+
+    string again = "y";
+
+    //keep going while the player still has money and they reply "y" to keep playing
+    while ((myPlayer.get_money() > 0) && (again == "y")) {
+
+        int bet;
+        cout << "You have $" << myPlayer.get_money() << ". Enter bet: ";
+        cin >> bet;
+
+        Hand playerHand = Hand();
+        playerHand.addCard(Card()); //draw a random card for the player
+
+        cout << "Your cards: " << endl;
+        playerHand.printCards();
+
+        string another;
+        cout << "Your total is " << playerHand.get_total() << ". Do you want another card (y/n)? ";
+        cin >> another;
+
+        while (another == "y") {
+            Card newCard = Card();
+            playerHand.addCard(newCard); //draw a random card for the player
+            cout << "New card:" << endl;
+            newCard.printCards();
+
+            cout << "Your cards: " << endl;
+            playerHand.printCards();
+
+            cout << "Your total is " << playerHand.get_total() << ". Do you want another card (y/n)? ";
+            cin >> another;
+        }
+
+        Hand dealerHand = Hand();
+        dealerHand.addCard(Card()); //draw a random card for the dealer
+
+        cout << "Dealer's cards: " << endl;
+        dealerHand.printCards();
+
+        cout << "The dealer's total is " << dealerHand.get_total() << endl;
+
+        while (dealerHand.get_total() < DEALER_THRESHOLD) {
+            Card newCard = Card();
+            dealerHand.addCard(newCard); //draw a random card for the dealer
+            cout << "New card:" << endl;
+            newCard.printCards();
+
+            cout << "Dealer's cards: " << endl;
+            dealerHand.printCards();
+
+            cout << "The dealer's total is " << dealerHand.get_total() << endl;
+        }
+
+        if (playerHand.bust()) {
+            //the player busted; dealer won
+            cout << "Too bad. You lose $" << bet << endl;
+            myPlayer.lose(bet);
+        }
+
+        else if (dealerHand.bust()) {
+            //dealer busted and player didn't; player won
+            cout << "Nice! You win $" << bet << endl;
+            myPlayer.gain(bet);
+        }
+
+        else if (playerHand.get_total() == dealerHand.get_total()) {
+            //tie
+            cout << "Tie. You don't gain or lose any money." << endl;
+        }
+
+        else if (playerHand.get_total() < dealerHand.get_total()) {
+            //dealer outscored player; dealer won
+            cout << "Too bad. You lose $" << bet << endl;
+            myPlayer.lose(bet);
+        }
+
+        else if (playerHand.get_total() > dealerHand.get_total()) {
+            //player outscored dealer; player won
+            cout << "Nice! You win $" << bet << endl;
+            myPlayer.gain(bet);
+        }
+
+        else {
+            //error
+            cout << "Error in programming logic!" << endl;
+            std::exit(0);
+        }
+
+        /* Here we will log the game to an external file */
+
+        ofstream logfile;
+        logfile.open ("./gamelog.txt");
+        logfile << "Hello World";
+        logfile.close();
+
+        /**************************************************/
+
+        cout << "Play again (y/n)?";
+        cin >> again;
+    }
+
     return 0;
+
 }
